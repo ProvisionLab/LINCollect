357ee75d625d9ed7daacd71c8eb0c76dbe1dcdeb
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2016-05-12 10:58:36 +0300

    Changed password

--- DbBootstrapper.bat
+++ DbBootstrapper.bat
@@ -1,3 +1,3 @@
 cd database
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 5.7\my.ini" "-uroot" "-p" "--default-character-set=utf8" "--host=localhost" "--user=root" "--password=Nabustest123" < db_bootstrapper.sql
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 5.7\my.ini" "-uroot" "-p" "--default-character-set=utf8" "--host=localhost" "--user=root" "--password=00000" < db_bootstrapper.txt
 PAUSE
