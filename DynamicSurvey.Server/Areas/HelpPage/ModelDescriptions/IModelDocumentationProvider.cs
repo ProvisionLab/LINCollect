8e2d9606e01ee53dbe42228f36c8e2e692fb0ab9
graymp <graymp@example.com>
2016-05-18 20:19:27 +0300

    restore package

---
+++ DynamicSurvey.Server/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace DynamicSurvey.Server.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
