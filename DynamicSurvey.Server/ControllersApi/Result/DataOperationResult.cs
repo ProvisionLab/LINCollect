671d33781f351d5455b06441ff774091f3c070bc
graymp <graymp@example.com>
2016-05-20 21:47:38 +0300

    api for surveys

---
+++ DynamicSurvey.Server/ControllersApi/Result/DataOperationResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicSurvey.Server.ControllersApi.Result
+{
+	public class DataOperationResult<TData> : OperationResultBase
+	{
+		public TData[] Data { get; set; }
+
+		public DataOperationResult() : base(200)
+		{
+
+		}
+	}
+}
