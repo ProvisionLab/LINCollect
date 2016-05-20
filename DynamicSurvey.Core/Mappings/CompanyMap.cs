52c9d1e44e8a92c2dfa57e67d306134c2bcd8ea7
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2016-05-20 14:39:58 +0300

    Integrated nhibernate/fluent nhibernate

---
+++ DynamicSurvey.Core/Mappings/CompanyMap.cs
@@ -0,0 +1,18 @@
+﻿using DynamicSurvey.Core.Entities;
+using FluentNHibernate.Mapping;
+
+namespace DynamicSurvey.Core.Mappings
+{
+    public class CompanyMap : ClassMap<Company>
+    {
+        public CompanyMap()
+        {
+            Id(it => it.Id);
+            Map(it => it.Name);
+            Map(it => it.Address);
+            Map(it => it.PhoneNumber);
+            Map(it => it.PostalCode);
+            References(it => it.City).Column("City_Id");
+        }
+    }
+}
---
+++ DynamicSurvey.Core/SessionStorage/SessionStorageContainerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace DynamicSurvey.Core.SessionStorage
+{
+    public static class SessionStorageContainerFactory
+    {
+        public static ISessionStorageContainer GetStorageContainer()
+        {
+            if (HttpContext.Current == null)
+            {
+                return new ThreadSessionStorageContainer();
+            }
+
+            return new HttpSessionStorageContainer();
+        }
+    }
+}
