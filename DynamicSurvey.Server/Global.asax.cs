2b351771992331c07df00b050260fae385af3fab
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2016-05-22 10:42:20 +0300

    some changes

--- DynamicSurvey.Server/Global.asax.cs
+++ DynamicSurvey.Server/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System.Web.Http;
+﻿using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.SessionState;
 using DynamicSurvey.Core;
 using DynamicSurvey.Core.SessionStorage;
 using DynamicSurvey.Server.Infrastructure;
@@ -11,13 +13,12 @@ namespace DynamicSurvey.Server
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             PersistenceContext.SetConnectionString(SurveysConfig.ConnectionString);
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             DependencyResolver.SetResolver(new NinjectDependencyResolver());
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -32,7 +33,7 @@ namespace DynamicSurvey.Server
 
         protected void Application_PostAuthorizeRequest()
         {
-            System.Web.HttpContext.Current.SetSessionStateBehavior(System.Web.SessionState.SessionStateBehavior.Required);
+            HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
         }
     }
 }
