d240e0dd06066605fd00e1eca6f02bce7415a065
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2016-05-23 00:04:39 +0300

    Added theme folder

---
+++ DynamicSurvey.Server/Controllers/AnswersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using DynamicSurvey.Server.DAL.Filters;
+using DynamicSurvey.Server.DAL.Repositories;
+
+namespace DynamicSurvey.Server.Controllers
+{
+    public class AnswersController : Controller
+    {
+        private readonly IAnswersRepository _answersRepository;
+
+        public AnswersController(IAnswersRepository answersRepository)
+        {
+            _answersRepository = answersRepository;
+        }
+
+        public ActionResult Index(ulong surveyId)
+        {
+            var reports = _answersRepository.GetReports(new SurveyReportFilter
+            {
+                SurveyId = surveyId
+            });
+            return View(reports);
+        }
+    }
+}
