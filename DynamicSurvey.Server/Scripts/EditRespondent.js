25b4b749f50ee55f0f1c14266306a58f5d27a858
Oleg Ivanov <oleg.ivanov.nabusteam@gmail.com>
2016-05-25 19:41:48 +0300

    Adding and swapping rows

--- DynamicSurvey.Server/Scripts/EditRespondent.js
+++ DynamicSurvey.Server/Scripts/EditRespondent.js
@@ -1,10 +1,49 @@
 ﻿var EditRespondent = function() {
 
+    var addRow = function() {
+        var rowTemplate = $(".answer-choice-row:first").clone(true);
+        $(".answer-choice-row:last").after(rowTemplate);
+    };
+
+    var handleEditQuestionModal = function() {
+        $("#question").ckeditor();
+
+        $("#addRow").click(function() {
+            addRow();
+        });
+
+        $(".move-row-up").click(function() {
+            var row = $(this).closest(".answer-choice-row");
+            if (row.is(".answer-choice-row:first")) {
+                return;
+            }
+
+            var beforeRow = row.prev(".answer-choice-row");
+            beforeRow.before(row);
+        });
+
+        $(".move-row-down").click(function() {
+            var row = $(this).closest(".answer-choice-row");
+            if (row.is(".answer-choice-row:last")) {
+                return;
+            }
+
+            var nextRow = row.next(".answer-choice-row");
+            nextRow.after(row);
+        });
+
+        $(".delete-row").click(function() {
+            $(this).closest(".answer-choice-row").remove();
+        });
+
+        $("#editQuestionModal").modal("show");
+    };
+
     var initEditRespondentPage = function() {
         var questionAction = $("#questionAction").val();
 
         if (questionAction) {
-            $("#editQuestionModal").modal("show");
+            handleEditQuestionModal();
         }
     };
 
