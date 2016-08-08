f08518220104e918b714e28d2d438dd221a9798f
YOsipov <os_evg@inbox.ru>
2016-08-08 17:43:23 +0300

    All my changes

---
+++ DynamicSurvey.Server/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DynamicSurvey.Server.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+    public class AdminAuth
+    {
+        public static bool IsAutorizedFlag = false;
+    }
+}
---
+++ DynamicSurvey.Server/Scripts/EditRelation.js
@@ -0,0 +1,12 @@
+var EditRelation = function() {
+    var initEditRelationPage = function() {
+
+
+
+    };
+    return {
+        init: function() {
+            initEditRelationPage();
+        }
+    };
+}();
